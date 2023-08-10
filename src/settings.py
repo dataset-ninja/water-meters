44b13336723b6cedc7a337ce93c17e68aed143be
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-10 19:45:27 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Water Meters"
 PROJECT_NAME_FULL: Optional[str] = "Water Meters Dataset"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -80,6 +81,7 @@ def get_settings():
 
     settings = {
         "project_name": PROJECT_NAME,
+        "hide_dataset": HIDE_DATASET,
         "license": LICENSE,
         "applications": APPLICATIONS,
         "category": CATEGORY,
